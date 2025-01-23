5f3aab799efcdaf48f2d0062f5402c81892ade85
Horizon1026 <lijiapeng_atlantis@163.com>
2025-01-24 00:02:15 +0800

    update code

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cd build/
-# ./test_feature_point_detector
-# ./test_feature_descriptor
-# ./test_feature_line_detector
+./test_feature_point_detector
+./test_feature_descriptor
+./test_feature_line_detector
 ./test_nn_feature_point_detector
 cd ..
